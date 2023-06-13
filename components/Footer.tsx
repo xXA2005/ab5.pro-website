a5667dad982d91dbcad97a585a635782ad83d29b
xXA2005 <A2005BALILA@gmail.com>
2023-06-13 16:35:33 +0300

    nikko github

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <section id='thanks' className='max-w-[1140px] mx-auto px-4 py-24 h-14 lg:h-[10vh]'>
-      Thanks for <a href='https://terrorist.store/' target='_blank' className='font-semibold text-blue-500'>nikko</a> for hot domain ab5.wtf
+      Thanks for <a href='https://github.com/n-ikko' target='_blank' className='font-semibold text-blue-500'>nikko</a> for hot domain ab5.wtf
     </section>
   )
 }
