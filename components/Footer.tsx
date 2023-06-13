af06ef974b872fda006034b7dcfe3c62f75362db
xXA2005 <A2005BALILA@gmail.com>
2023-06-13 15:37:32 +0300

    t

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='max-w-[1140px] mx-auto px-4 py-24 h-14 lg:h-[10vh]'>
+    <section id='thanks' className='max-w-[1140px] mx-auto px-4 py-24 h-14 lg:h-[10vh]'>
       Thanks for <a href='https://terrorist.store/' target='_blank' className='font-semibold text-blue-500'>nikko</a> for hot domain ab5.wtf
-    </div>
+    </section>
   )
 }
 
