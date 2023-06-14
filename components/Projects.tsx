46cad656b9865f3bebe25c2fe2ac456a9a241f06
xXA2005 <A2005BALILA@gmail.com>
2023-06-14 17:58:49 +0300

    gogo

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import SectionTitle from './SectionTitle'
 import Image from 'next/image'
-import { ab5, hcaptcha, mc } from '@/public'
+import { ab5, hcaptcha, mc, gogen } from '@/public'
 import { SiPypi, SiNpm } from 'react-icons/si'
 import { BsCodeSlash } from 'react-icons/bs'
 
@@ -107,6 +107,29 @@ const Projects = () => {
                   <BsCodeSlash/>
                 </a>
               </div>
+            </div>
+          </div>
+        </div>
+        {/* project 4 */}
+        <div className='w-full flex flex-col items-center justify-center gap-28 mt-10'>
+          <div className='flex flex-col xl:flex-row gap-6'>
+            <a 
+              className='w-full xl:w-1/2 h-auto relative group'
+              // href='https://github.com/xXA2005/minecraft-botter-with-proxy-support' 
+              target='_blank'>
+              <div>
+                <Image className='w-full h-full object-contain' src={gogen} alt={'gogen'}/>
+              </div>
+            </a>
+            <div className='w-full xl:w-1/2 flex flex-col gap-6 lg:justify-between items-end text-right xl:-ml-16 z-10'>
+              <div>
+                <p className='text-blue-500 text-sm tracking-wide'>Featured Project</p>
+                <h3 className='text-2xl font-bold'>Golang discord account maker</h3>
+              </div>
+              <p className='bg-gray-800 text-sm md:text-base p-2 md:p-6 rounded-md'>make discord accounts</p>
+              <ul className='text-xs md:text-sm tracking-wide flex gap-2 md:gap-5 justify-between text-gray-400'>
+                <li>golang</li>
+              </ul>
             </div>
           </div>
         </div>
--- public/index.ts
+++ public/index.ts
@@ -1,7 +1,8 @@
 import hcaptcha from './hcaptcha.png'
 import mc from './mc.png'
 import ab5 from './ab5.png'
+import gogen from './gogen.png'
 
 
 
-export { hcaptcha, ab5, mc }
+export { hcaptcha, ab5, mc, gogen }
