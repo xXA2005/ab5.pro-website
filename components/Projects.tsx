eeb8306c1c82266051a4c2cd0f29d9619caf782b
xXA2005 <A2005BALILA@gmail.com>
2023-07-13 22:29:42 +0300

    a

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,133 +1,173 @@
-import React from 'react'
-import SectionTitle from './SectionTitle'
-import Image from 'next/image'
-import { ab5, hcaptcha, mc, gogen } from '@/public'
-import { SiPypi, SiNpm } from 'react-icons/si'
-import { BsCodeSlash } from 'react-icons/bs'
+import React from "react";
+import SectionTitle from "./SectionTitle";
+import Image from "next/image";
+import { ab5, hcaptcha, mc, gogen } from "@/public";
+import { SiPypi, SiNpm } from "react-icons/si";
+import { BsCodeSlash } from "react-icons/bs";
 
 const Projects = () => {
   return (
-    <section id="projects" className='max-w-[1440px] mx-auto lg:px-20 py-24'>
-      <SectionTitle title='Projects' titleNumber='2.'/>
-      <div className='w-full flex flex-col items-center justify-between gap-28 mt-10'>
+    <section id="projects" className="max-w-[1440px] mx-auto lg:px-20 py-24">
+      <SectionTitle title="Projects" titleNumber="2." />
+      <div className="w-full flex flex-col items-center justify-between gap-28 mt-10">
         {/* project 1 */}
-        <div className='w-full flex flex-col items-center justify-center gap-28 mt-10'>
-          <div className='flex flex-col xl:flex-row gap-6'>
-            <a 
-              className='w-full xl:w-1/2 h-auto relative group'
-              href='https://pypi.org/project/ab5' 
-              target='_blank'>
+        <div className="w-full flex flex-col items-center justify-center gap-28 mt-10">
+          <div className="flex flex-col xl:flex-row gap-6">
+            <a
+              className="w-full xl:w-1/2 h-auto relative group"
+              href="https://pypi.org/project/ab5"
+              target="_blank"
+            >
               <div>
-                <Image className='w-full h-full object-contain' src={ab5} alt={'ab5'}/>
+                <Image
+                  className="w-full h-full object-contain"
+                  src={ab5}
+                  alt={"ab5"}
+                />
               </div>
             </a>
-            <div className='w-full xl:w-1/2 flex flex-col gap-6 lg:justify-between items-end text-right xl:-ml-16 z-10'>
+            <div className="w-full xl:w-1/2 flex flex-col gap-6 lg:justify-between items-end text-right xl:-ml-16 z-10">
               <div>
-                <p className='text-blue-500 text-sm tracking-wide'>Featured Project</p>
-                <h3 className='text-2xl font-bold'>Ab5 python package</h3>
+                <p className="text-blue-500 text-sm tracking-wide">
+                  Featured Project
+                </p>
+                <h3 className="text-2xl font-bold">Ab5 python package</h3>
               </div>
-              <p className='bg-gray-800 text-sm md:text-base p-2 md:p-6 rounded-md'>hot python package for making hot CLI apps</p>
-              <ul className='text-xs md:text-sm tracking-wide flex gap-2 md:gap-5 justify-between text-gray-400'>
+              <p className="bg-gray-800 text-sm md:text-base p-2 md:p-6 rounded-md">
+                hot python package for making hot CLI apps
+              </p>
+              <ul className="text-xs md:text-sm tracking-wide flex gap-2 md:gap-5 justify-between text-gray-400">
                 <li>python</li>
                 <li>pypi</li>
               </ul>
-              <div className='text-2xl flex gap-4'>
-                <a 
-                className='hover:text-blue-500 duration-300'
-                href='https://pypi.org/project/ab5/'
-                target='_blank'>
-                  <SiPypi/>
+              <div className="text-2xl flex gap-4">
+                <a
+                  className="hover:text-blue-500 duration-300"
+                  href="https://pypi.org/project/ab5/"
+                  target="_blank"
+                >
+                  <SiPypi />
                 </a>
-                <a 
-                className='hover:text-blue-500 duration-300'
-                href='https://files.pythonhosted.org/packages/b6/19/284022b4389e98b32dbb406c6d5570a38060effb8a56a7fe9fcc6d75007e/ab5-0.7.tar.gz'
-                target='_blank'>
-                  <BsCodeSlash/>
+                <a
+                  className="hover:text-blue-500 duration-300"
+                  href="https://files.pythonhosted.org/packages/b6/19/284022b4389e98b32dbb406c6d5570a38060effb8a56a7fe9fcc6d75007e/ab5-0.7.tar.gz"
+                  target="_blank"
+                >
+                  <BsCodeSlash />
                 </a>
               </div>
             </div>
           </div>
         </div>
         {/* project 2 */}
-        <div className='w-full flex flex-col items-center justify-center gap-28 mt-10'>
-          <div className='flex flex-col xl:flex-row-reverse gap-6'>
-            <a 
-              className='w-full xl:w-1/2 h-auto relative group'
-              // href='https://pypi.org/project/ab5' 
+        <div className="w-full flex flex-col items-center justify-center gap-28 mt-10">
+          <div className="flex flex-col xl:flex-row-reverse gap-6">
+            <a
+              className="w-full xl:w-1/2 h-auto relative group"
+              // href='https://pypi.org/project/ab5'
               // target='_blank'
-              >
+            >
               <div>
-                <Image className='w-full h-full object-contain' src={hcaptcha} alt={'hcaptcha'}/>
+                <Image
+                  className="w-full h-full object-contain"
+                  src={hcaptcha}
+                  alt={"hcaptcha"}
+                />
               </div>
             </a>
-            <div className='w-full xl:w-1/2 flex flex-col gap-6 lg:justify-between items-end text-right z-10'>
+            <div className="w-full xl:w-1/2 flex flex-col gap-6 lg:justify-between items-end text-right z-10">
               <div>
-                <p className='text-blue-500 text-sm tracking-wide'>Featured Project</p>
-                <h3 className='text-2xl font-bold'>Hcaptcha solver</h3>
+                <p className="text-blue-500 text-sm tracking-wide">
+                  Featured Project
+                </p>
+                <h3 className="text-2xl font-bold">Hcaptcha solver</h3>
               </div>
-              <p className='bg-gray-800 text-sm md:text-base p-2 md:p-6 rounded-md xl:-mr-16'>Fast requests based hcaptcha solver with AI</p>
-              <ul className='text-xs md:text-sm tracking-wide flex gap-2 md:gap-5 justify-between text-gray-400'>
+              <p className="bg-gray-800 text-sm md:text-base p-2 md:p-6 rounded-md xl:-mr-16">
+                Fast requests based hcaptcha solver with AI
+              </p>
+              <ul className="text-xs md:text-sm tracking-wide flex gap-2 md:gap-5 justify-between text-gray-400">
                 <li>python</li>
               </ul>
             </div>
           </div>
         </div>
         {/* project 3 */}
-        <div className='w-full flex flex-col items-center justify-center gap-28 mt-10'>
-          <div className='flex flex-col xl:flex-row gap-6'>
-            <a 
-              className='w-full xl:w-1/2 h-auto relative group'
-              href='https://github.com/xXA2005/minecraft-botter-with-proxy-support' 
-              target='_blank'>
+        <div className="w-full flex flex-col items-center justify-center gap-28 mt-10">
+          <div className="flex flex-col xl:flex-row gap-6">
+            <a
+              className="w-full xl:w-1/2 h-auto relative group"
+              href="https://github.com/xXA2005/minecraft-botter-with-proxy-support"
+              target="_blank"
+            >
               <div>
-                <Image className='w-full h-full object-contain' src={mc} alt={'mc'}/>
+                <Image
+                  className="w-full h-full object-contain"
+                  src={mc}
+                  alt={"mc"}
+                />
               </div>
             </a>
-            <div className='w-full xl:w-1/2 flex flex-col gap-6 lg:justify-between items-end text-right xl:-ml-16 z-10'>
+            <div className="w-full xl:w-1/2 flex flex-col gap-6 lg:justify-between items-end text-right xl:-ml-16 z-10">
               <div>
-                <p className='text-blue-500 text-sm tracking-wide'>Featured Project</p>
-                <h3 className='text-2xl font-bold'>Minecraft Raider</h3>
+                <p className="text-blue-500 text-sm tracking-wide">
+                  Featured Project
+                </p>
+                <h3 className="text-2xl font-bold">Minecraft Raider</h3>
               </div>
-              <p className='bg-gray-800 text-sm md:text-base p-2 md:p-6 rounded-md'>Raid minecraft servers with funny controllable robots</p>
-              <ul className='text-xs md:text-sm tracking-wide flex gap-2 md:gap-5 justify-between text-gray-400'>
+              <p className="bg-gray-800 text-sm md:text-base p-2 md:p-6 rounded-md">
+                Raid minecraft servers with funny controllable robots thru your
+                terminal
+              </p>
+              <ul className="text-xs md:text-sm tracking-wide flex gap-2 md:gap-5 justify-between text-gray-400">
                 <li>node.js</li>
                 <li>mineflayer.js</li>
               </ul>
-              <div className='text-2xl flex gap-4'>
-                <a 
-                className='hover:text-blue-500 duration-300'
-                href='https://www.npmjs.com/package/mineflayer'
-                target='_blank'>
-                  <SiNpm/>
+              <div className="text-2xl flex gap-4">
+                <a
+                  className="hover:text-blue-500 duration-300"
+                  href="https://www.npmjs.com/package/mineflayer"
+                  target="_blank"
+                >
+                  <SiNpm />
                 </a>
-                <a 
-                className='hover:text-blue-500 duration-300'
-                href='https://github.com/xXA2005/minecraft-botter-with-proxy-support'
-                target='_blank'>
-                  <BsCodeSlash/>
+                <a
+                  className="hover:text-blue-500 duration-300"
+                  href="https://github.com/xXA2005/minecraft-botter-with-proxy-support"
+                  target="_blank"
+                >
+                  <BsCodeSlash />
                 </a>
               </div>
             </div>
           </div>
         </div>
         {/* project 4 */}
-        <div className='w-full flex flex-col items-center justify-center gap-28 mt-10'>
-          <div className='flex flex-col xl:flex-row gap-6'>
-            <a 
-              className='w-full xl:w-1/2 h-auto relative group'
-              // href='https://github.com/xXA2005/minecraft-botter-with-proxy-support' 
-              target='_blank'>
+        <div className="w-full flex flex-col items-center justify-center gap-28 mt-10">
+          <div className="flex flex-col xl:flex-row-reverse gap-6">
+            <a
+              className="w-full xl:w-1/2 h-auto relative group"
+              // href='https://github.com/xXA2005/minecraft-botter-with-proxy-support'
+              target="_blank"
+            >
               <div>
-                <Image className='w-full h-full object-contain' src={gogen} alt={'gogen'}/>
+                <Image
+                  className="w-full h-full object-contain"
+                  src={gogen}
+                  alt={"gogen"}
+                />
               </div>
             </a>
-            <div className='w-full xl:w-1/2 flex flex-col gap-6 lg:justify-between items-end text-right xl:-ml-16 z-10'>
+            <div className="w-full xl:w-1/2 flex flex-col gap-6 lg:justify-between items-end text-right xl:-ml-16 z-10">
               <div>
-                <p className='text-blue-500 text-sm tracking-wide'>Featured Project</p>
-                <h3 className='text-2xl font-bold'>Golang discord account maker</h3>
+                <p className="text-blue-500 text-sm tracking-wide">
+                  Featured Project
+                </p>
+                <h3 className="text-2xl font-bold">Discord token generator</h3>
               </div>
-              <p className='bg-gray-800 text-sm md:text-base p-2 md:p-6 rounded-md'>make discord accounts</p>
-              <ul className='text-xs md:text-sm tracking-wide flex gap-2 md:gap-5 justify-between text-gray-400'>
+              <p className="bg-gray-800 text-sm md:text-base p-2 md:p-6 rounded-md">
+                make discord accounts
+              </p>
+              <ul className="text-xs md:text-sm tracking-wide flex gap-2 md:gap-5 justify-between text-gray-400">
                 <li>golang</li>
               </ul>
             </div>
@@ -135,7 +175,7 @@ const Projects = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
--- components/Xp.tsx
+++ components/Xp.tsx
@@ -1,44 +1,71 @@
-import React, { useState } from 'react'
-import SectionTitle from './SectionTitle'
-import Langs from './exp/Langs'
-import Frameworks from './exp/Frameworks'
-import Tools from './exp/Tools'
+import React, { useState } from "react";
+import SectionTitle from "./SectionTitle";
+import Langs from "./exp/Langs";
+import Frameworks from "./exp/Frameworks";
+import Tools from "./exp/Tools";
 
 const Xp = () => {
-  const [openLangs, setOpenLangs] = useState(true)
-  const [openFrameworks, setOpenFrameworks] = useState(false)
-  const [openTools, setOpenTools] = useState(false)
+  const [openLangs, setOpenLangs] = useState(true);
+  const [openFrameworks, setOpenFrameworks] = useState(false);
+  const [openTools, setOpenTools] = useState(false);
   const handleLangs = () => {
-    setOpenLangs(true)
-    setOpenFrameworks(false)
-    setOpenTools(false)
-  }
+    setOpenLangs(true);
+    setOpenFrameworks(false);
+    setOpenTools(false);
+  };
   const handleFrameworks = () => {
-    setOpenLangs(false)
-    setOpenFrameworks(true)
-    setOpenTools(false)
-  }
+    setOpenLangs(false);
+    setOpenFrameworks(true);
+    setOpenTools(false);
+  };
   const handleTools = () => {
-    setOpenLangs(false)
-    setOpenFrameworks(false)
-    setOpenTools(true)
-  }
+    setOpenLangs(false);
+    setOpenFrameworks(false);
+    setOpenTools(true);
+  };
   return (
-    <section className='mx-auto py-10 lg:py-24 px-4 max-w-[768px]' id='xp'>
-        <SectionTitle title='Experince' titleNumber='1.'/>
-        <div className='w-full mt-10 flex flex-col md:flex-row gap-16'>
-            <ul className='md:w-32 flex flex-col'>
-                <li onClick={handleLangs} className={`${openLangs ? "border-l-blue-500 text-blue-500" : "border-l-[#112240] text-gray-400"} border-l-2 bg-transparent hover:bg-[#112240] py-3 text-sm  cursor-pointer duration-300 px-8 font-medium`}>Langs</li>
-                <li onClick={handleFrameworks} className={`${openFrameworks ? "border-l-blue-500 text-blue-500" : "border-l-[#112240] text-gray-400"} border-l-2 bg-transparent hover:bg-[#112240] py-3 text-sm  cursor-pointer duration-300 px-8 font-medium`}>Frameworks</li>
-                <li onClick={handleTools} className={`${openTools ? "border-l-blue-500 text-blue-500" : "border-l-[#112240] text-gray-400"} border-l-2 bg-transparent hover:bg-[#112240] py-3 text-sm  cursor-pointer duration-300 px-8 font-medium`}>Tools</li>
-                {/* <li onClick={handleAchivements} className='border-l-[#112240] text-gray-400 border-l-2 bg-transparent hover:bg-[#112240] py-3 text-sm  cursor-pointer duration-300 px-8 font-medium'>Achivements</li> */}
-            </ul>
-            {openLangs && <Langs/>}
-            {openFrameworks && <Frameworks/>}
-            {openTools && <Tools/>}
-        </div>
+    <section className="mx-auto py-10 lg:py-24 px-4 max-w-[768px]" id="xp">
+      <SectionTitle title="Experience" titleNumber="1." />
+      <div className="w-full mt-10 flex flex-col md:flex-row gap-16">
+        <ul className="md:w-32 flex flex-col">
+          <li
+            onClick={handleLangs}
+            className={`${
+              openLangs
+                ? "border-l-blue-500 text-blue-500"
+                : "border-l-[#112240] text-gray-400"
+            } border-l-2 bg-transparent hover:bg-[#112240] py-3 text-sm  cursor-pointer duration-300 px-8 font-medium`}
+          >
+            Langs
+          </li>
+          <li
+            onClick={handleFrameworks}
+            className={`${
+              openFrameworks
+                ? "border-l-blue-500 text-blue-500"
+                : "border-l-[#112240] text-gray-400"
+            } border-l-2 bg-transparent hover:bg-[#112240] py-3 text-sm  cursor-pointer duration-300 px-8 font-medium`}
+          >
+            Frameworks
+          </li>
+          <li
+            onClick={handleTools}
+            className={`${
+              openTools
+                ? "border-l-blue-500 text-blue-500"
+                : "border-l-[#112240] text-gray-400"
+            } border-l-2 bg-transparent hover:bg-[#112240] py-3 text-sm  cursor-pointer duration-300 px-8 font-medium`}
+          >
+            Tools
+          </li>
+          {/* <li onClick={handleAchivements} className='border-l-[#112240] text-gray-400 border-l-2 bg-transparent hover:bg-[#112240] py-3 text-sm  cursor-pointer duration-300 px-8 font-medium'>Achivements</li> */}
+        </ul>
+        {openLangs && <Langs />}
+        {openFrameworks && <Frameworks />}
+        {openTools && <Tools />}
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Xp
+export default Xp;
