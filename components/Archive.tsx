2028857ae786d4ba10f6324ea2f79e722702be21
xXA2005 <A2005BALILA@gmail.com>
2023-07-13 22:40:35 +0300

    a

--- components/Archive.tsx
+++ components/Archive.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from 'react';
-import RepoCard from './RepoCard'
+import { useEffect, useState } from "react";
+import RepoCard from "./RepoCard";
+import SectionTitle from "./SectionTitle";
 
 type Repo = {
   name: string;
@@ -10,55 +11,57 @@ type Repo = {
   html_url: string;
 };
 
-
 const RepoList = () => {
   const [repos, setRepos] = useState([]);
-  const [showMore, setShowMore] = useState(false)
+  const [showMore, setShowMore] = useState(false);
 
   useEffect(() => {
     const fetchRepos = async () => {
       try {
-        const response = await fetch('https://api.github.com/users/xXA2005/repos');
+        const response = await fetch(
+          "https://api.github.com/users/xXA2005/repos"
+        );
         const data = await response.json();
         const filteredRepos = data.filter((repo: Repo) => !repo.fork);
         setRepos(filteredRepos);
       } catch (error) {
-        console.error('Error fetching repos:', error);
+        console.error("Error fetching repos:", error);
       }
     };
 
     fetchRepos();
   }, []);
 
-  const visibleRepos = showMore ? repos : repos.slice(0, 3)
+  const visibleRepos = showMore ? repos : repos.slice(0, 3);
 
   return (
-    <div className='max-w-[1140px] mx-auto px-4 py-24'>
-      <div className='w-full flex flex-col items-center'>
-            <h2 className='text-3xl font-semibold'>
-                Other Projects
-            </h2>
-            <p className='text-sm text-blue-500'>View Projects</p>
-        </div>
-        <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mt-10 lgl:px-10'>
+    <div className="max-w-[1140px] mx-auto px-4 py-24">
+      <SectionTitle title="Other Projects" titleNumber="3." />
+      <div className="w-full flex flex-col items-center">
+        <p className="text-sm text-blue-500">View Projects</p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mt-10 lgl:px-10">
         {visibleRepos.map((repo: Repo) => (
-    <RepoCard
-      key={repo.name}
-      title={repo.name}
-      description={repo.description}
-      stars={repo.stargazers_count}
-      lang={repo.language}
-      url={repo.html_url}
-    />
-  ))}
-        </div>
-        <div className='mt-12 flex items-center justify-center'>
-          <button onClick={() => setShowMore((prevState) => !prevState)} className='w-36 h-12 rounded-md text-blue-500 text-[13px] border border-blue-500 hover:bg-[#112240]'>{showMore ? 'Show Less' : 'Show More'}</button>
-        </div>
+          <RepoCard
+            key={repo.name}
+            title={repo.name}
+            description={repo.description}
+            stars={repo.stargazers_count}
+            lang={repo.language}
+            url={repo.html_url}
+          />
+        ))}
+      </div>
+      <div className="mt-12 flex items-center justify-center">
+        <button
+          onClick={() => setShowMore((prevState) => !prevState)}
+          className="w-36 h-12 rounded-md text-blue-500 text-[13px] border border-blue-500 hover:bg-[#112240]"
+        >
+          {showMore ? "Show Less" : "Show More"}
+        </button>
+      </div>
     </div>
   );
 };
 
 export default RepoList;
-
-
--- components/exp/Langs.tsx
+++ components/exp/Langs.tsx
@@ -1,24 +1,53 @@
-import { motion } from 'framer-motion'
-import { DiPython } from 'react-icons/di'
-import { TbBrandGolang, TbBrandCSharp } from 'react-icons/tb'
-import { DiJavascript1, DiJava } from 'react-icons/di'
+import { motion } from "framer-motion";
+import { DiPython } from "react-icons/di";
+import { TbBrandGolang, TbBrandCSharp } from "react-icons/tb";
+import { DiJavascript1, DiJava } from "react-icons/di";
 
 const langs = () => {
   return (
-    <motion.div initial={{opacity:0}}
-                animate={{opacity:1}}
-                transition={{delay:0.1}}
-                className='w-full'>
-      <h3 className='flex gap-1 font-medium text-xl pb-4'>Programming Languages</h3>
-      <ul className='flex gap-2 text-base flex-col'>
-        <li className='flex flex-row gap-1'><span className='text-blue-500'><DiPython/></span>Python <span className='text-blue-400'>pro max</span></li>
-        <li className='flex flex-row gap-1'><span className='text-blue-500'><TbBrandGolang/></span>Go <span className='text-blue-400'>pro</span></li>
-        <li className='flex flex-row gap-1'><span className='text-blue-500'><DiJavascript1/></span>Javascript/Typescript <span className='text-blue-400'>pro</span></li>
-        <li className='flex flex-row gap-1'><span className='text-blue-500'><TbBrandCSharp/></span>C#<span className='text-blue-400'>basics</span></li>
-        <li className='flex flex-row gap-1'><span className='text-blue-500'><DiJava/></span>Java<span className='text-blue-400'>basics</span></li>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.1 }}
+      className="w-full"
+    >
+      <h3 className="flex gap-1 font-medium text-xl pb-4">
+        Programming Languages
+      </h3>
+      <ul className="flex gap-2 text-base flex-col">
+        <li className="flex flex-row gap-1">
+          <span className="text-blue-500">
+            <DiPython />
+          </span>
+          Python <span className="text-blue-400">pro max ultra</span>
+        </li>
+        <li className="flex flex-row gap-1">
+          <span className="text-blue-500">
+            <TbBrandGolang />
+          </span>
+          Go <span className="text-blue-400">pro max</span>
+        </li>
+        <li className="flex flex-row gap-1">
+          <span className="text-blue-500">
+            <DiJavascript1 />
+          </span>
+          Javascript/Typescript <span className="text-blue-400">pro</span>
+        </li>
+        <li className="flex flex-row gap-1">
+          <span className="text-blue-500">
+            <TbBrandCSharp />
+          </span>
+          C#<span className="text-blue-400">basics</span>
+        </li>
+        <li className="flex flex-row gap-1">
+          <span className="text-blue-500">
+            <DiJava />
+          </span>
+          Java<span className="text-blue-400">basics</span>
+        </li>
       </ul>
     </motion.div>
-  )
-}
+  );
+};
 
-export default langs
+export default langs;
